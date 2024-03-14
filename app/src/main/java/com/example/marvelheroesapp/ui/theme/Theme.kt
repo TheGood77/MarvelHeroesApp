77764c716cf5f5369bfb602e2091eccf26a88b72
TheGood77 <thegood77@bk.ru>
2024-03-14 13:57:32 +0600

    fix navbar color

--- app/src/main/java/com/example/marvelheroesapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/marvelheroesapp/ui/theme/Theme.kt
@@ -25,22 +25,11 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun MarvelHeroesAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -57,7 +46,8 @@ fun MarvelHeroesAppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = BackgroundColor.toArgb()
+            window.navigationBarColor = BackgroundColor.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
@@ -65,6 +55,6 @@ fun MarvelHeroesAppTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
     )
 }
